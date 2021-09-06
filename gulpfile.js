a44e036937f693b139799445191a559721c3c180
Vladimir <vovik261096@mail.ru>
2021-09-06 14:04:49 +0700

    готов к разработке

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,19 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass')(require('sass'));
-
+const cleanCSS =require('gulp-clean-css');
+const autoprefixer = require("gulp-autoprefixer")
+const rename = require("gulp-rename");
 // Static server
 gulp.task('server', function() {
+    
     browserSync.init({
         server: {
             baseDir: "src"
         }
     });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('styles',function() {
